ae12399a5fa6ec5969aa8f5947eddefa543ae44c
Sakib Fakir <fakirsakib22232>
2024-12-22 21:25:28 +0600

    extra2 photo added done

--- src/Pages/ExtraPage2.jsx
+++ src/Pages/ExtraPage2.jsx
@@ -3,13 +3,46 @@ import React from "react";
 function ExtraPage2() {
   return (
     <div className="">
-      <h1>Ancient Egypt Tools and Weapons</h1>
-      {/* set a slider */}
-      {/* used */}
-      <section className="grid grid-cols-4">
+      <h1>Top artifact </h1>
+      {/* {gave me details all artifact} */}
+
+      <section className="grid md:grid-cols-4 grid-cols-2 gap-4">
+
+        <div>
+            <img src='/KingTutankhamunsMaskegpt.jpg' alt="egpt"/>
+            <p>Name: King Tutankhamun's Mask </p>
+            
+        </div>
+
+        <div>
+            <img src='/TheCrownJewelsoftheUnitedKingdomuk.jpg' alt="england"/>
+            <p>The Crown Jewels of the United Kingdom</p>
+        </div>
+        <div>
+            <img src='/TheDeadSeaScrollspleastine.jpg' alt="pleastine"/>
+            <p>The DeadScrolls</p>
+        </div>
+        <div>
+            <img src='/TheMagnaCartaengland.jpg' alt="egpt"/>
+            <p>The Magna Carta england. </p>
+        </div>
+        <div>
+            <img src='/TheRosettaStone.jpg' alt="egpt"/>
+            <p>The Rosetta Stone </p>
+        </div>
+        <div>
+            <img src='/TheSphinxofGizaegpt.jpg' alt="egpt"/>
+            <p>The Sphinx of Giza egpt</p>
+        </div>
         
         <div>
-            <img/>
+            <img src='/TheTerracottaArmychina.jpg' alt="egpt"/>
+            <p>The Terracotta Army china.jpg</p>
+        </div>
+
+        <div>
+            <img src='/TheElginMarblesgreec.jpg' />
+            <p>The Elgin Marbles  greec</p>
         </div>
 
         
