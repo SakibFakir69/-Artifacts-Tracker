06fb6136782ff7fc56c2a148ed488fc3a32d895a
Sakib Fakir <fakirsakib22232>
2024-12-25 01:57:06 +0600

    feture page api done

--- src/Pages/FeaturedPage.jsx
+++ src/Pages/FeaturedPage.jsx
@@ -1,11 +1,34 @@
 
 
 
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 function FeaturedPage() {
+
+
+  const [ data , setdata ] = useState([]);
+
+  useEffect(()=>{
+    axios.get(`http://localhost:5000/higestLikeCount`)
+    .then((res)=>{
+      setdata(res.data);
+    })
+
+
+
+
+  },[])
+  console.log(data);
+
+
+
   return (
-    <div>FeaturedPage</div>
+    <div>
+      
+      asdf {data.length}
+      
+
+    </div>
   )
 }
 
